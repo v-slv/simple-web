f254c5fc38f3e5eba90fa148cdef7ec3c7b82516
Silviu-Ioan Vilcu <silviuioanvilcu@gmail.com>
2023-01-19 23:49:47 +0200

    Update pipeline.js

--- tools/pipeline.js
+++ tools/pipeline.js
@@ -1,9 +1,10 @@
 
 /**
- * A pipeline is a simple, working in isolation, function that allows you to automatically perform synchronous operations on an object whenever its properties change
- * It uses a Proxy object that traps all of the changes on the original data object that will trigger the operations execetion in the given order
- * @param {*} data 
- * @param {*} operations 
+ * A pipeline is a simple, working in isolation, scope that allows you to automatically perform synchronous operations on a given object whenever its properties change
+ * It uses a Proxy object to trap all of the changes of the original data object that will trigger the operations execution in the given order
+ * @param {*} data any object
+ * @param {*} operations an array of functions with a unique parameter representing the current state of the processed data object, that could implement a specific logic
+ * and could return a processed data object that could be furtherly processed by the next operation
  * @returns 
  */
 export function pipeline(data, operations) {
